ea81defd5d938365f008d437258be93e7ad855e9
Aniket Thorat <thorataniket777@gmail.com>
2020-10-15 23:20:03 -0700

    web ui

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ Created on Wed Oct 14 19:34:07 2020
 
 @author: NEXUS
 """
-
-from packages import dataMining
+from flask import Flask,render_template
 from datamining.dataMining import DataMining as dm
 
-dm.text_mining()
+app=Flask("__name__")
+
+@app.route("/",methods=["GET","POST"])
+def Home():
+    dm.text_mining()
+    return render_template("Home.html")
+
+
+              
+
+
+
+app.run(debug=True)
